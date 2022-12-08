403825189a990cac2d6328385d206172885e9d7e
marcio sansone <msansone@gmail.com>
2022-12-08 19:05:15 +0000

    created user list

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'msansone/my-stock-php',
+        'pretty_version' => 'dev-release',
+        'version' => 'dev-release',
+        'reference' => 'a12c10ea324c0e9dece7b6a9b7393f79e5c077a6',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'msansone/my-stock-php' => array(
+            'pretty_version' => 'dev-release',
+            'version' => 'dev-release',
+            'reference' => 'a12c10ea324c0e9dece7b6a9b7393f79e5c077a6',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
